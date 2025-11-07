3d7d62de53507715561cdab99882157d4913a76d
Faisal Dani Noto Legowo <151006064+goodman97@users.noreply.github.com>
2025-11-07 15:48:26 +0700

    Update crypto_algorithms.py
    camelia

--- models/crypto_algorithms.py
+++ models/crypto_algorithms.py
@@ -1,22 +1,23 @@
 from hashlib import shake_128
-from Crypto.Cipher import AES
+from Cryptodome.Cipher import Camellia
 import base64
 
 # SHAKE-128 untuk password hashing
 def hash_password(password):
-    return shake_128(password.encode()).hexdigest(32)
+    return shake_128(password.encode()).hexdigest(64)
 
-# simulasi Camellia pakai AES untuk sementara
+# Enkripsi dengan Camellia (ECB mode)
 def camellia_encrypt(data, key):
-    key = key.ljust(16, '0')[:16].encode()
-    cipher = AES.new(key, AES.MODE_ECB)
+    key = key.ljust(16, '0')[:16].encode() 
+    cipher = Camellia.new(key, Camellia.MODE_ECB)
     pad_len = 16 - len(data) % 16
     data += chr(pad_len) * pad_len
     enc = cipher.encrypt(data.encode())
     return base64.b64encode(enc).decode()
 
+# Dekripsi dengan Camellia (ECB mode)
 def camellia_decrypt(enc_data, key):
     key = key.ljust(16, '0')[:16].encode()
-    cipher = AES.new(key, AES.MODE_ECB)
+    cipher = Camellia.new(key, Camellia.MODE_ECB)
     dec = cipher.decrypt(base64.b64decode(enc_data)).decode()
     return dec[:-ord(dec[-1])]
